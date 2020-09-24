57e8a44123b7bfa906920d4c20c04d1aad2e9896
unknown <carezfr10@gmail.com>
2020-09-24 15:42:07 +0700

    second commit

---
+++ pages/shop/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>รายละเอียด</h1>
+    <h2>สินค้าหมายเลข {{ this.$route.params.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/shop/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Shop Page</h1>
+    <ul>
+      <li v-for="i in 10" :key="i">
+        Item Number {{ i }}
+        <nuxt-link :to="{ name: 'shop-id', params: { id: i } }"
+          >รายละเอียด</nuxt-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
